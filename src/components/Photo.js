f2cec8f52483a38bed5396de6cf40f8f29ae92ac
donutwizard666 <thebeardedwarrior@icloud.com>
2019-10-10 20:13:21 -0700

    fixed photo isse

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,12 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 
-
+const PhotoWrapper = styled.div`
+    @media (max-width: 750px) {
+        width:100%;
+    }
+    img {
+        max-width: 100%;
+    }
+`;
 
 function Image(props) {
     return (
-        
-            <img src={props.image} alt="NGC 7714: Starburst after Galaxy Collision"/>
-        
+    <PhotoWrapper className="photo-wrapper">
+        <img src={props.image} alt="NGC 7714: Starburst after Galaxy Collision"/>
+    </PhotoWrapper>    
     )
 }
 export default Image;
